5679624ab2363a04f6c32834e77c81484ec5dcfc
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-08-20 16:57:19 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Hompage from './components/Homepage';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hompage/>}/>
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
