5ab55406582140ef076129077558ad4759a588e7
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-08-22 16:32:59 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
 import Homepage from './Homepage';
 
 function App() {
   return (
     <Router basename="/blue-sky-studio">
+     <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
