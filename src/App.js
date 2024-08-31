b20410aba4b4d214b2ac3da3f6e4ef912c300639
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-09-01 07:46:32 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Homepage from './Homepage';
-
+import './App.css';
+import React, { useState } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/page/HomePage';
+import TopButton from './components/common/TopButton';
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import Loading from './components/animations/Loading';
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadingFinish = () => {
+    setIsLoading(false);
+  };
+
   return (
     <Router>
-     <Navbar />
+      {!isLoading && <Navbar />}
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Loading Component={HomePage} onLoadingFinish={handleLoadingFinish} />} />
+        {/* <Route path="*" element={<Page404 />} /> */}
       </Routes>
+      <TopButton />
+      {!isLoading && <Footer />}
     </Router>
   );
 }
