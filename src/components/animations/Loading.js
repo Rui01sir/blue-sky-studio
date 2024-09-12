bda05d109649f9c772f14bec91e68649ad265b4c
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-09-12 21:57:39 +0800

    Add files via upload

--- src/components/animations/Loading.js
+++ src/components/animations/Loading.js
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import ProgressBar from './PorgressBar';
+import React, { useEffect } from 'react';
 
 const Loading = ({ Component, onLoadingFinish }) => {
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-    setTimeout(() => {
-        setIsLoading(false);
-    onLoadingFinish();
-    }, 2000);
-    }, [onLoadingFinish]);
+  useEffect(() => {
+    // 假設動畫時間為3秒，3秒後觸發 onLoadingFinish
+    const timer = setTimeout(() => {
+      onLoadingFinish();
+    }, 3000); // 這個時間根據你的動畫時間進行調整
 
-    return (
-        <div>
-            {isLoading ? ( 
-                <div className="loading-page">
-                    <img src={`${process.env.PUBLIC_URL}/img/logo-loading.svg`} alt='loading-logo'></img>
-                    <ProgressBar />
-                </div>
-            ) :( 
-                <Component/>
-            )}
-        </div>
-    );
+    return () => clearTimeout(timer);
+  }, [onLoadingFinish]);
+
+  return (
+    <div className="loading-container">
+    <div className="svg-container">
+      <img src={`${process.env.PUBLIC_URL}/img/Group-10.svg`} alt="Group-10-logo" className="loading-svg"></img>
+      <img src={`${process.env.PUBLIC_URL}/img/Group-11.svg`} alt="Group-11-logo" className="loading-svg"></img>
+    </div>
+  </div>
+  );
 };
 
 export default Loading;
--- src/components/page/HomePage.js
+++ src/components/page/HomePage.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  useEffect(() => {
+    const boxes = document.querySelectorAll('.homepage-banner__text, .homepage-banner__img, .homepage-text h4');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log(`正在动画: ${entry.target.className}`);
+                
+                if (entry.target.classList.contains('homepage-banner__text')) {
+                    entry.target.classList.add('animate__slideInLeft');
+                } else if (entry.target.classList.contains('homepage-banner__img')) {
+                    entry.target.classList.add('animate__fadeInUp');
+                } else if (entry.target.classList.contains('homepage-text h4')) {
+                    entry.target.classList.add('animate__fadeInUp');
+                }
+                entry.target.classList.add('animate__animated');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 });
+
+    boxes.forEach(box => {
+        observer.observe(box);
+    });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+      navigate(path);
+  };
+
+  useEffect(() => {
+      window.scrollTo(0, 0);
+  }, []);
 
   return (
-    <div className="container">
-      <>s</>
+    <div>
+      <div className='homepage-container'>
+        <div className='homepage-banner'>
+          <div className='homepage-banner__text'>
+            <h1>青空設計</h1>
+            <h4>是一家專注於 #創新設計<br/>#行銷策略的專業設計公司</h4>
+          </div>
+          <div className='homepage-banner__img'>
+             <img src={`${process.env.PUBLIC_URL}/img/homepage-banner.png`} alt="homepage-banner"></img>
+          </div>
+        </div>
+        <div className='homepage-text'>
+          <h4>我們的核心服務包括  #平面設計 #網頁設計 #行銷策略<br/>
+          我們的目標是為客戶提供全方位的品牌體驗，從概念到實現，助力品牌成長與成功。</h4>
+        </div>
+      </div>
     </div>
   );
 }
