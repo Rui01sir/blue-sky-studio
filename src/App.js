5bc0d487840b895b9482f3983ff4a4227b1f32c8
睿 <122418084+Rui01sir@users.noreply.github.com>
2025-02-23 02:14:48 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/page/HomePage';
 import Link from './components/page/Link';
+// import LoveCard from './components/page/LoveCard';
 import TopButton from './components/common/TopButton';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
